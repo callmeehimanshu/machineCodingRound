52a1708046b2cbfeb91ad099cab768cedd901368
Himanshu <himanshuagl18@gmail.com>
2025-04-15 02:46:09 +0530

    todo created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import CounterTest from './Counter/CounterTest'
 import StopWatchTest from './StopWatch/StopWatchTest'
 import './App.css'
+import TodoTest from './ToDo/TodoTest'
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
     <>
     <CounterTest/>
      <StopWatchTest/>
-     
+     <TodoTest/>
     </>
   )
 }
